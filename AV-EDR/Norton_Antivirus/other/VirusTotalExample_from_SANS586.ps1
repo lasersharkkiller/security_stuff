24b4bd9b427041f7a28c39beb402632d13fb5211
lasersharkkiller <35998923+lasersharkkiller@users.noreply.github.com>
2023-03-08 14:16:39 -0500

    Delete VirusTotalExample_from_SANS586.ps1

--- AV-EDR/Norton_Antivirus/other/VirusTotalExample_from_SANS586.ps1
+++
@@ -1,7 +0,0 @@
-$notepadProc = Get-Process notepad
-$notepadProc | Add-Member -MemberType ScriptProperty -Name VTPositives -Value {
-$fileHash = Get-FileHash ($notepadProc.Path) | Select-Object -ExpandProperty Hash
-$uri = "https://www.virustotal.com/vtapi/v2/file/report?apikey=$apikey&resource=$fileHash"
-Invoke-RestMethod -Uri $uri |Select-Object -ExpandProperty positives
-
-#C:> $notepadProc.VTPositives
