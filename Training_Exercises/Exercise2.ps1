58e906426525b96f34712f17a59f6ff7d7f4dbd8
lasersharkkiller <35998923+lasersharkkiller@users.noreply.github.com>
2023-09-12 16:23:43 -0400

    Update Exercise2.ps1

--- Training_Exercises/Exercise2.ps1
+++ Training_Exercises/Exercise2.ps1
@@ -9,8 +9,8 @@ $pickRandom = Get-Random -Minimum 0 -Maximum 55
 
 $Exercise2exe = $strarry[$pickRandom]
 
-#This assume putty.exe is in the same folder as your script
-Copy-Item putty.exe $strarry[$pickRandom]
+#This assume apt170Unsigned.exe is in the same folder as your script
+Copy-Item apt170Unsigned.exe $strarry[$pickRandom]
 #Run
 Start-Sleep -Milliseconds 3000
 Invoke-Expression -Command ".\$($Exercise2exe)"
