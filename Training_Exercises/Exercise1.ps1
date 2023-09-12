885864e247cda0f2d1f86bcc301fcdd3b1963dc6
lasersharkkiller <35998923+lasersharkkiller@users.noreply.github.com>
2023-09-12 11:44:54 -0400

    Create Exercise1.ps1

---
+++ Training_Exercises/Exercise1.ps1
@@ -0,0 +1,16 @@
+#############################################################
+##########################Exercise #1########################
+#############################################################
+
+#Array of core services
+$strarry = @("smss.exe", "wininit.exe", "runtimebroker.exe","taskhostw.exe","winlogon.exe","csrss.exe","services.exe","svchost.exe","lsaiso.exe","lsass.exe","explorer.exe")
+
+$pickRandom = Get-Random -Minimum 0 -Maximum 10
+
+$Exercise1exe = $strarry[$pickRandom]
+
+#This assume putty.exe is in the same folder as your script
+Copy-Item putty.exe $strarry[$pickRandom]
+#Run
+Start-Sleep -Milliseconds 3000
+Invoke-Expression -Command ".\$($Exercise1exe)"
